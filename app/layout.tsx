941c8a8c44153b65f8c7cf36c86f442b80779eb2
unknown <giovanni.basilva@gmail.com>
2026-06-08 21:39:14 -0300

    feat: shadcn ui components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Navbar } from '@/components/layout/Navbar'
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: 'Bolão Copa do Mundo FIFA 2026',
@@ -13,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={cn("font-sans", geist.variable)}>
       <body>
         <Navbar />
         <main className="max-w-6xl mx-auto px-4 pb-20">
